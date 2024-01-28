695c8fd25b297604217b2c964323e3efe637d0de
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-28 21:32:44 +0100

    creato classe Gioco con legame a classe Prodotto (is a) e inserite variabili

---
+++ Models/Cibo.php
@@ -0,0 +1,12 @@
+<?php
+require_once '/Prodotto.php';
+
+class Gioco extends Prodotto
+{
+
+    protected $nome;
+    protected $lifeStage;
+    protected $colore;
+    protected $larghezza;
+    protected $altezza;
+}

9baca4a7039a484b8d79a99daa268af8fafdade7
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-28 18:52:38 +0100

    inserito funzioni get e set per variabili della classe. creato constructor

--- Models/Prodotto.php
+++ Models/Prodotto.php
@@ -7,4 +7,39 @@ class Prodotto
     protected $stock;
 
     protected $categoria;
+
+    public function __construct($_disponibilità, $_codiceProdotto, $_stock, $_categoria)
+    {
+        $this->setDisponibilità($_disponibilità);
+        $this->setCodiceProdotto($_codiceProdotto);
+        $this->setStock($_stock);
+        $this->categoria = $_categoria;
+    }
+
+    protected function getDisponibilità()
+    {
+        return $this->disponibilità;
+    }
+    protected  function setDisponibilità($__disponibilità)
+    {
+        return ucfirst(strtolower($__disponibilità));
+    }
+
+    protected function getCodiceProdotto()
+    {
+        return $this->disponibilità;
+    }
+    protected  function setCodiceProdotto($__CodiceProdotto)
+    {
+        return ucfirst(strtolower($__CodiceProdotto));
+    }
+
+    protected function getStock()
+    {
+        return $this->stock;
+    }
+    protected  function setStock($__Stock)
+    {
+        return ucfirst(strtolower($__Stock));
+    }
 }
